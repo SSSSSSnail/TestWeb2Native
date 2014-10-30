82065c33f2752e67dcb419b9ea5a6fcaf1583afc
Snail <warlocke@163.com>
2014-10-30 15:24:52 +0800

    init
    无中生有

---
+++ TestWeb2Native/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestWeb2Native
+//
+//  Created by Snail on 30/10/14.
+//  Copyright (c) 2014 Snail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestWeb2Native/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestWeb2Native
+//
+//  Created by Snail on 30/10/14.
+//  Copyright (c) 2014 Snail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
